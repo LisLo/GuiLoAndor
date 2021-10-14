a4beab3f846cbff59bddd2670dbb3a0495058e1b
David <lisaloerinci@gmail.com>
2021-10-14 17:14:55 +0200

    Clearing flake8

--- Main/main.py
+++ Main/main.py
@@ -2,13 +2,8 @@ import os
 import sys
 
 from kivy.app import App
-# from kivy.properties import StringProperty
-from kivy.properties import ObjectProperty
-# from kivy.uix.popup import Popup
-# from kivy.uix.label import Label
 from kivy.core.window import Window
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
 
 # find root dir
 root_path = os.path.split((os.path.dirname(__file__)))[0]
@@ -16,18 +11,8 @@ root_path = os.path.split((os.path.dirname(__file__)))[0]
 sys.path.append(root_path)
 from Source.Config.get_input import GetInput
 
-Window.size = (1000,700)
+Window.size = (1000, 700)
 GUI = Builder.load_file("main.kv")
-
-
-# class Games(Screen):
-    #btn1 = ObjectProperty()
-    #btn2 = ObjectProperty()
-    #btn3 = ObjectProperty()
-
-
-# ms = ScreenManager()
-# ms.add_widget(Games(name="games"))
 
 
 class StartApp(GetInput, App):

d57badedb720b8fb2b87db6c82de6eefd4791c2f
David <lisaloerinci@gmail.com>
2021-10-19 21:00:12 +0200

    First Screen

--- Main/main.py
+++ Main/main.py
@@ -1,20 +1,19 @@
 import os
 import sys
+import kivy 
 
 from kivy.app import App
-from kivy.core.window import Window
-from kivy.lang import Builder
-
 # find root dir
 root_path = os.path.split((os.path.dirname(__file__)))[0]
 # set root
 sys.path.append(root_path)
 from Source.Config.get_input import GetInput
 
-Window.size = (1000, 700)
-GUI = Builder.load_file("main.kv")
 
+class TestApp(App):
+    pass
 
+'''
 class StartApp(GetInput, App):
     def __init__(self):
         super().__init__(__file__)
@@ -23,7 +22,8 @@ class StartApp(GetInput, App):
         Window.clearcolor = (1, 1, 1, 1)
         # print(self.A1)
         return GUI
-
+'''
 
 if __name__ == "__main__":
-    StartApp().run()
+    #StartApp().run()
+    TestApp().run()
